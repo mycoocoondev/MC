62021ba48c3c22c58fd950d4ff887418f8fb4d0c
Agustin Sobral <asobralr@gmail.com>
2021-01-21 19:05:44 -0300

    Mixing colors

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { Shaders, GLSL, Node } from "gl-react";
 import { Surface } from "gl-react-expo";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const colors = {
+  color1: [248,88,255],
+  color2: [87, 0, 93],
+  color3: [81,1,186],
+  color4: [85,0,191]
+}
+
 // in gl-react you need to statically define "shaders":
 const shaders = Shaders.create({
   helloGL: {
 // This is our first fragment shader in GLSL language (OpenGL Shading Language)
 // (GLSL code gets compiled and run on the GPU)
-    frag: GLSL`
+frag: GLSL`
 precision highp float;
 varying vec2 uv;
+uniform vec3 start, end;
 void main() {
-  gl_FragColor = vec4(uv.x, uv.y, 0.5, 1.0);
+  vec3 color = mix(end, start, 0.5);
+  gl_FragColor = vec4(color, 1.0);
 }
 `
 // the main() function is called FOR EACH PIXELS
@@ -27,9 +39,10 @@ void main() {
 
 export default class TabTwoScreen extends Component {
   render() {
+    const { color1, color2, color3, color4 } = colors;
     return (
-        <Surface style={{width: 300, height: 300}}>
-          <Node shader={shaders.helloGL} />
+        <Surface style={{width: windowWidth, height: windowHeight}}>
+          <Node shader={shaders.helloGL} uniforms={{start: color1.map(v=>v/255), end: color2.map(v=>v/255)}} />
         </Surface>
     );
 // Surface creates the canvas, an area of pixels where you can draw.
